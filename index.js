d45c4bd981904f8aa888afc19fe0072b5f3fd4e2
iines <2268050k@student.gla.ac.uk>
2021-02-12 16:35:14 +0000

    preliminary layout done

---
+++ index.js
@@ -0,0 +1,42 @@
+orderArr = ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "zero"];
+// get time and convert to string to push to database
+var start = new Date();
+var time = start.getDate().toString();
+var hours = start.getHours().toString();
+var min = start.getMinutes().toString();
+var sec = start.getSeconds().toString();
+time = time.concat(".", hours,".", min, ".", sec);
+
+//rearrange array to get random order:
+function shuffle(arr) {
+  var current = arr.length, temp, rand;
+  while (0 !== current) {
+    rand = Math.floor(Math.random() * current);
+    current -= 1;
+    temp = arr[current];
+    arr[current] = arr[rand];
+    arr[rand] = temp;
+  }
+  return arr;
+}
+
+// get array of numbers
+function getRandOrder() {
+  randArr = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+  // rearrange targets to get random order
+  randArr = this.shuffle(randArr);
+}
+
+function changeNumPad(){
+  this.getRandOrder();
+  document.getElementById("one").setAttribute("value", randArr[0]);
+  document.getElementById("two").setAttribute("value", randArr[1]);
+  document.getElementById("three").setAttribute("value", randArr[2]);
+  document.getElementById("four").setAttribute("value", randArr[3]);
+  document.getElementById("five").setAttribute("value", randArr[4]);
+  document.getElementById("six").setAttribute("value", randArr[5]);
+  document.getElementById("seven").setAttribute("value", randArr[6]);
+  document.getElementById("eight").setAttribute("value", randArr[7]);
+  document.getElementById("nine").setAttribute("value", randArr[8]);
+  document.getElementById("zero").setAttribute("value", randArr[9]);
+}
