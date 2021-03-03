5d3602a1b82d5bb2d14576fe5a15d27244e2f6d1
iines <2268050k@student.gla.ac.uk>
2021-03-03 15:58:30 +0000

    cleaned up js code, fixed layout to be relative to screen size

--- index.js
+++ index.js
@@ -45,37 +45,45 @@ enteredPasscode = [];
 text = [];
 
 function onButtonClick(enteredNum){
-  if (enteredNum == "back"){
-    enteredPasscode.pop();
-    text.pop();
-    document.getElementById("passcode").innerHTML = text.join("");
-  }
-  else if (enteredNum == "enter"){
-    // check if the length of the entered passcode matches the passcode
-    if (enteredPasscode.length == passcode.length){
-      // if yes, check if the passcode matches
-      if (compareArrays(enteredPasscode, passcode)){
-        setTimeout(function() {alert("Correct passcode, device unlocked.");},1);
-        this.changeNumPad();
-        enteredPasscode = [];
-        text = [];
-        document.getElementById("passcode").innerHTML = text.join("");
-      }
-      else{
-        setTimeout(function() {alert("Incorrect passcode, try again.");},1);
-        this.changeNumPad();
-        enteredPasscode = [];
-        text = [];
-        document.getElementById("passcode").innerHTML = text.join("");
-      }
+  text.push("*");
+  document.getElementById("passcode").innerHTML = text.join("");
+  // save the number that was just entered
+  enteredPasscode.push(enteredNum);
+  console.log(enteredPasscode);
+}
+
+function onBackClick(){
+  enteredPasscode.pop();
+  text.pop();
+  document.getElementById("passcode").innerHTML = text.join("");
+}
+
+function wrongPasscode(){
+  setTimeout(function() {alert("Incorrect passcode, try again.");},1);
+  this.changeNumPad();
+  enteredPasscode = [];
+  text = [];
+  document.getElementById("passcode").innerHTML = text.join("");
+}
+
+function onEnter(){
+  // check if the length of the entered passcode matches the passcode
+  if (enteredPasscode.length == passcode.length){
+    // if yes, check if the passcode matches
+    if (compareArrays(enteredPasscode, passcode)){
+      setTimeout(function() {alert("Correct passcode, device unlocked.");},1);
+      this.changeNumPad();
+      enteredPasscode = [];
+      text = [];
+      document.getElementById("passcode").innerHTML = text.join("");
+    }
+    // if no, wrong passcode
+    else{
+      this.wrongPasscode();
     }
   }
   else{
-    text.push("*");
-    document.getElementById("passcode").innerHTML = text.join("");
-    // save the number that was just entered
-    enteredPasscode.push(enteredNum);
-    console.log(enteredPasscode);
+    this.wrongPasscode();
   }
 }
 
